2e6dd6d26d1bf56290ae8e6809595cba37955e4e
Bart-15 <brat.tabusao@gmail.com>
2022-02-01 18:24:39 +0800

    add client

---
+++ client/components/Footer.js
@@ -0,0 +1,12 @@
+import {Container} from '@mui/material'
+const Footer = () => {
+    return ( 
+        <Container>
+            <footer>
+                 <h1>This is the footer.</h1>
+            </footer>
+        </Container>
+     );
+}
+ 
+export default Footer ;
---
+++ client/pages/todos/index.js
@@ -0,0 +1,113 @@
+import React, {useState, useEffect} from 'react';
+import {Container, Grid, TextField} from '@mui/material'
+import TodoItem from './TodoItem'
+import { toast } from 'react-toastify';
+import axios from 'axios'
+
+
+const Todos = ({BASE_URL}) => {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [counter, setCounter] = useState(0);
+   
+    useEffect(() => {
+        fetchTodos();
+   
+    },[])
+
+    const fetchTodos = async() => {
+        const res = await axios.get(`${BASE_URL}/api/v1/todos`)
+        const data = await res.data.todos;
+        setCounter(0)
+        setTodos(data);
+    }
+
+    const deleteTodo = async(id) => {
+        try {
+            toast.warn('Todo Deleted Successfully!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                });
+          await axios.delete(`${BASE_URL}/api/v1/todo/${id}`)
+          fetchTodos();
+        }catch(e) {
+          console.log(e)
+        }
+    }
+
+    const toggleCompleted = async(id) => {
+       setCounter(counter + 1)
+       if(counter === 1) {
+        await axios.patch(`${BASE_URL}/api/v1/todo/completed/${id}`)
+        fetchTodos();
+       }
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        if(!todo) {
+            return alert("Todo field is required.")
+        }
+
+        const newTodo = {
+            title: todo
+        }
+        toast.success('Todo Created Successfully!', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+        });
+        await axios.post(`${BASE_URL}/api/v1/todos`, newTodo)
+        setTodo("")
+        fetchTodos();
+    }
+
+
+
+
+
+    return ( 
+        <Container>
+            <Grid container spacing={4}>
+                <Grid item xs={12} sm={12} md={6} lg={8} alignItems="center">
+                    <form onSubmit={handleSubmit}>
+                        <TextField type="text" name="todo" fullWidth label="Enter Todo" id="fullWidth" value={todo} onChange={e => setTodo(e.target.value)} />
+                    </form>
+                </Grid>
+            </Grid>
+            {
+                todos.map((todo) => {
+                    return (
+                        <TodoItem 
+                            key={todo._id} 
+                            todo={todo}
+                            deleteTodo={deleteTodo}
+                            toggleCompleted={toggleCompleted}
+                        />
+                    )
+                })
+            }
+        </Container>
+     );
+}
+ 
+export const getServerSideProps = async() => {
+    const BASE_URL = process.env.API
+
+    return {
+        props: {
+            BASE_URL
+        }
+    }
+}
+
+export default Todos;
